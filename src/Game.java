dd9f21deb979374d6b962a9f233cfb06ab06d0fb
Stormagheddon <Hayden@smashdev.com>
2016-03-06 11:22:25 -0800

    Added getGame and createGame methods

--- src/Game.java
+++ src/Game.java
@@ -2,40 +2,8 @@ import java.util.ArrayList;
 
 public class Game {
 	public static void main (String args[]){
-		Item key1 = new Item("Key", "Iron Key");
-		Item key2 = new Item("Key", "Rusty Key");
-		Room room1 = new Room("The Courtyard");
-		Room room2 = new Room("The south end of the Main Hall");
-		Room room3 = new Room("The Library");
-		Room room4 = new Room("Hidden Room");
-		Room room5 = new Room("The Basement");
-		Room room6 = new Room("The north end of the Main Hall");
-		Room room7 = new Room("Room 7");
-		Room room8 = new Room("Room 8");
-		Room room9 = new Room("Room 9");
-		Room room10 = new Room("Room 10");
-		Room room11 = new Room("Room 11");
-		Room room12 = new Room("Room 12");
-		Door door1 = new Door(room1.NorthWall, room2.SouthWall, key1);
-		Door door2 = new Door(room2.EastWall, room9.WestWall, key2);
-		Door door3 = new Door(room2.WestWall, room3.EastWall);
-		Door door4 = new Door(room4.EastWall, room3.WestWall);
-		Door door5 = new Door(room4.NorthWall, room5.SouthWall);
-		Door door6 = new Door(room2.NorthWall, room6.SouthWall);
-		Door door7 = new Door(room6.EastWall, room8.WestWall);
-		Door door8 = new Door(room9.EastWall, room10.WestWall);
-		Door door9 = new Door(room6.WestWall, room7.EastWall);
-		Door door10 = new Door(room10.SouthWall, room11.NorthWall);
-		Door door11 = new Door(room11.EastWall, room12.WestWall);
-		
-		room1.Ground.add(key1);
-		room1.Ground.add(new Item("Sword", "Sword"));
-		room1.Ground.add(new Item("Lantern", "Lantern"));
-		room1.Ground.add(new Item("Scroll", "Scroll"));
-		room2.Ground.add(key2);
-		
-		Character character = new Character();
-		character.Location = room1;
+	
+		Character character = getGame();
 		
 		System.out.println("Welcome to the game!");
 		System.out.println("Type 'help' to see the commands.");
@@ -209,5 +177,49 @@ public class Game {
 		else{
 			System.out.println("You didn't find: " + itemName);
 		}
+	}
+	
+	private static Character getGame(){
+		//TODO add game save loading logic
+		return createGame();
+	}
+	
+	private static Character createGame(){
+		Item key1 = new Item("Key", "Iron Key");
+		Item key2 = new Item("Key", "Rusty Key");
+		Room room1 = new Room("The Courtyard");
+		Room room2 = new Room("The south end of the Main Hall");
+		Room room3 = new Room("The Library");
+		Room room4 = new Room("Hidden Room");
+		Room room5 = new Room("The Basement");
+		Room room6 = new Room("The north end of the Main Hall");
+		Room room7 = new Room("Room 7");
+		Room room8 = new Room("Room 8");
+		Room room9 = new Room("Room 9");
+		Room room10 = new Room("Room 10");
+		Room room11 = new Room("Room 11");
+		Room room12 = new Room("Room 12");
+		Door door1 = new Door(room1.NorthWall, room2.SouthWall, key1);
+		Door door2 = new Door(room2.EastWall, room9.WestWall, key2);
+		Door door3 = new Door(room2.WestWall, room3.EastWall);
+		Door door4 = new Door(room4.EastWall, room3.WestWall);
+		Door door5 = new Door(room4.NorthWall, room5.SouthWall);
+		Door door6 = new Door(room2.NorthWall, room6.SouthWall);
+		Door door7 = new Door(room6.EastWall, room8.WestWall);
+		Door door8 = new Door(room9.EastWall, room10.WestWall);
+		Door door9 = new Door(room6.WestWall, room7.EastWall);
+		Door door10 = new Door(room10.SouthWall, room11.NorthWall);
+		Door door11 = new Door(room11.EastWall, room12.WestWall);
+		
+		room1.Ground.add(key1);
+		room1.Ground.add(new Item("Sword", "Sword"));
+		room1.Ground.add(new Item("Lantern", "Lantern"));
+		room1.Ground.add(new Item("Scroll", "Scroll"));
+		room2.Ground.add(key2);
+		
+		Character character = new Character();
+		character.Location = room1;
+		
+		return character;
 	}
 }
